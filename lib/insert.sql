a71e6843ff25c5ec12fe7380c296258ffbcb25d4
Jared Patterson <jpwesley34@gmail.com>
2024-05-10 18:43:17 -0700

    Passes tests.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "Brown", "Smooth", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 7, "M", "Black", "Calm", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 23, "F", "Brown", "Bollywood", FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown",21, "M", "Brownish", "Sargey", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 15, "F", "Brown", "Hungry", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 103, "M", "Green", "Festive", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 16, "F", "White", "Wise", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 18, "F", "White and Red", "Mischievous", TRUE);
